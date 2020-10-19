78e3969b69198c07b642f90c549afa7e0a1cc6e0
jakjan95 <jakubjanusz95@gmail.com>
2020-10-19 17:30:31 +0200

    Added example of system use for homework

--- Homework/CoffeShopSystem/main.cpp
+++ Homework/CoffeShopSystem/main.cpp
@@ -11,5 +11,11 @@ int main() {
     auto americanaWithSoy = std::make_unique<AmericanaWithSoy>();
     std::cout << americanaWithSoy->getDescription() << " cost : " << americanaWithSoy->cost() << '\n';
 
+    // Example of system use for homework
+    // std::unique_ptr<Coffe> americanWithSoy = std::make_unique<Americana>();
+    // americanaWithSoy = std::make_unique<Soy>(std::move(americanaWithSoy));
+    // std::cout << americanaWithSoy->getDescription() << " cost : " << americanaWithSoy->cost() << '\n';
+
+    // make espressoWithDoubleMilkAndWhip
     return 0;
 }
